d6ab1285bebe100982f49b39d60ccbb51c36f02c
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-23 14:09:55 -0500

    header

---
+++ src/lib/components/_unique/Header/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+
+import Link from '../../Link/Link'
+
+export type HeaderProps = { title: string; tutorialLink: string; repoLink: string }
+
+export default function Header({ title, tutorialLink, repoLink }: HeaderProps) {
+  const tutLink = `https://jeremytenjo.com/tutorials/posts/${tutorialLink}`
+
+  return (
+    <Box component='header' sx={{ mb: '100px', textAlign: 'center' }}>
+      <h1>{title}</h1>
+      <Box>
+        Follow {` `}
+        <Link href={tutLink} newTab>
+          tutorial
+        </Link>
+        {` `}& {` `}
+        <Link href={repoLink} newTab>
+          repo
+        </Link>
+      </Box>
+    </Box>
+  )
+}
