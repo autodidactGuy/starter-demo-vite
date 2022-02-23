c21d1e3ff5c10adc0087c5dc095918d77ec55c24
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-02-23 11:40:10 -0500

    removed @useweb/use-install-prompt

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { UseInstallPromptProvider } from '@useweb/use-install-prompt'
 
 import Router from './pages/router'
 import Theme from './theme/theme'
 
 function App() {
   return (
-    <UseInstallPromptProvider>
-      <Theme>
-        <Router />
-      </Theme>
-    </UseInstallPromptProvider>
+    <Theme>
+      <Router />
+    </Theme>
   )
 }
 
