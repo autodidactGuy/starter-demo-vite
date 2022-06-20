015a3312aca298d4503ea485319a51fcc8e09f39
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-20 10:10:14 -0400

    root layout padding

--- src/lib/components/layouts/root.layout.tsx
+++ src/lib/components/layouts/root.layout.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Box from '@useweb/ui/Box'
 import { SnackbarProvider } from '@useweb/ui/Snackbar'
 
 export default function ShellLayout({ children }) {
   return (
     <>
       <SnackbarProvider>
-        <main>{children}</main>
+        <Box
+          component='main'
+          sx={{
+            p: 2,
+          }}
+        >
+          {children}
+        </Box>
       </SnackbarProvider>
     </>
   )
